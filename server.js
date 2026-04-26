08d2a517ad928fe8091208a6d53c0f1d2ccf6cf9
ceph0-labs <cephasmubothi85@gmail.com>
2026-04-26 08:02:54 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,5 @@
-const express = require("express");
-const app = express();
-
-// ✅ Serve frontend files
-app.use(express.static("public"));
-
-const PORT = process.env.PORT || 3000;
-
-const http = require("http");
-const { Server } = require("socket.io");
-
-const server = http.createServer(app);
-const io = new Server(server);
-
-// Multiplayer storage
 let players = {};
 
-// Socket connection
 io.on("connection", (socket) => {
 
     console.log("User connected:", socket.id);
@@ -58,31 +42,11 @@ io.on("connection", (socket) => {
             username: players[socket.id].username,
             amount: win.toFixed(2)
         });
-
-    socket.on("playerCashout", (data) => {
-    console.log(data.username + " cashed out " + data.amount);
-});
-
-    socket.on("players", (players) => {
-    const list = document.getElementById("playersList");
-
-    if (!list) return;
-
-    list.innerHTML = "";
-
-    players.forEach(player => {
-        const li = document.createElement("li");
-        li.textContent = player.username + " - Bet: " + player.bet;
-        list.appendChild(li);
     });
-});
+
     socket.on("disconnect", () => {
         delete players[socket.id];
         io.emit("players", Object.values(players));
     });
-});
 
-// IMPORTANT: use server.listen instead of app.listen
-server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
 });
